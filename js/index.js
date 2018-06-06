3ef0d51fed1b397daef390ff4ad169ad8251fa8d
shane <shane1041713@gmail.com>
2018-06-06 14:33:10 +0800

    fix

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(document).on('click', 'a[href^="#"]', function (event) {
 
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
+    }, 300);
 });
 
 window.onscroll = function () { scrollFunction() };
